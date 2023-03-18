62b3934642eb74a82eb5bff0fc1e3e47d9c99ed0
nourhene <nourhenemejri1@gmail.com>
2023-03-19 00:49:38 +0100

    update form

--- crawler/forms.py
+++ crawler/forms.py
@@ -2,4 +2,9 @@ from django import forms
 
 class QForm(forms.Form):
     query = forms.CharField(max_length=50)
+    content_type = forms.BooleanField(label='content type', required=False)
+    Media_size= forms.BooleanField(label='Media size', required=False)
+    video_format = forms.BooleanField(label='video format', required=False)
+    max_result = forms.BooleanField(label='maximum number of result', required=False)
+
     
